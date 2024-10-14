4e9ee956fa2625c2f114523beda52edcecebd9b4
Shahed Nasser <shahednasser@gmail.com>
2024-10-14 15:21:17 +0300

    update config to use ts

--- digital-product/medusa-config.ts
+++ digital-product/medusa-config.ts
@@ -1,28 +1,28 @@
-const { loadEnv, defineConfig, Modules } = require('@medusajs/framework/utils')
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
-loadEnv(process.env.NODE_ENV, process.cwd())
+loadEnv(process.env.NODE_ENV || "development", process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
-      storeCors: process.env.STORE_CORS,
-      adminCors: process.env.ADMIN_CORS,
-      authCors: process.env.AUTH_CORS,
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
   },
-  modules: {
-    digitalProductModuleService: {
+  modules: [
+    {
       resolve: "./modules/digital-product"
     },
-    [Modules.FULFILLMENT]: {
-      resolve: "@medusajs/medusa/fulfillment",
+    {
+      resolve: "@medusajs/fulfillment",
       options: {
         providers: [
           {
-            resolve: "@medusajs/medusa/fulfillment-manual",
+            resolve: "@medusajs/fulfillment-manual",
             id: "manual",
           },
           {
@@ -32,12 +32,12 @@ module.exports = defineConfig({
         ],
       },
     },
-    [Modules.NOTIFICATION]: {
-      resolve: "@medusajs/medusa/notification",
+    {
+      resolve: "@medusajs/notification",
       options: {
         providers: [
           {
-            resolve: "@medusajs/medusa/notification-local",
+            resolve: "@medusajs/notification-local",
             id: "local",
             options: {
               name: "Local Notification Provider",
@@ -47,5 +47,5 @@ module.exports = defineConfig({
         ],
       },
     },
-  }
+  ],
 })
--- restaurant-marketplace/medusa-config.ts
+++ restaurant-marketplace/medusa-config.ts
@@ -1,27 +1,27 @@
-const { loadEnv, defineConfig, Modules } = require('@medusajs/framework/utils')
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
-loadEnv(process.env.NODE_ENV, process.cwd())
+loadEnv(process.env.NODE_ENV || "development", process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
-      storeCors: process.env.STORE_CORS,
-      adminCors: process.env.ADMIN_CORS,
-      authCors: process.env.AUTH_CORS,
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
   },
-  modules: {
-    restaurantModuleService: {
+  modules: [
+    {
       resolve: "./modules/restaurant",
     },
-    deliveryModuleService: {
+    {
       resolve: "./modules/delivery"
     },
     // Recommended
-    // [Modules.WORKFLOW_ENGINE]: {
+    // {
     //   resolve: "@medusajs/workflow-engine-redis",
     //   options: {
     //     redis: {
@@ -29,5 +29,5 @@ module.exports = defineConfig({
     //     },
     //   },
     // },
-  }
+  ],
 })
