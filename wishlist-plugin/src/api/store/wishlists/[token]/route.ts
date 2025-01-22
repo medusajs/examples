2f7ca732616cb666976118e28973a9fd4859bea0
Shahed Nasser <shahednasser@gmail.com>
2025-01-22 11:03:23 +0200

    added check on sales channel for shared wishlist

--- wishlist-plugin/src/api/store/wishlists/[token]/route.ts
+++ wishlist-plugin/src/api/store/wishlists/[token]/route.ts
@@ -1,11 +1,18 @@
-import { MedusaRequest, MedusaResponse } from "@medusajs/framework";
+import { MedusaResponse, MedusaStoreRequest } from "@medusajs/framework";
 import { MedusaError } from "@medusajs/framework/utils";
 import { decode, JwtPayload } from "jsonwebtoken"
 
 export async function GET(
-  req: MedusaRequest,
+  req: MedusaStoreRequest,
   res: MedusaResponse
 ) {
+  if (!req.publishable_key_context?.sales_channel_ids.length) {
+    throw new MedusaError(
+      MedusaError.Types.INVALID_DATA,
+      "At least one sales channel ID is required to be associated with the publishable API key in the request header."
+    )
+  }
+  
   const decodedToken = decode(req.params.token) as JwtPayload
 
   if (!decodedToken.wishlist_id) {
@@ -29,6 +36,13 @@ export async function GET(
     throw new MedusaError(
       MedusaError.Types.NOT_FOUND,
       "No wishlist found"
+    )
+  }
+
+  if (data[0].sales_channel_id !== req.publishable_key_context.sales_channel_ids[0]) {
+    throw new MedusaError(
+      MedusaError.Types.INVALID_DATA,
+      "Wishlist does not belong to the request's sales channel"
     )
   }
 
