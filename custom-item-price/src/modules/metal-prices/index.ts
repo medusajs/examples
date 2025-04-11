4cabf4fa1b6852367aa7db13bb1222d3e97c41fd
Shahed Nasser <shahednasser@gmail.com>
2025-04-11 14:09:18 +0300

    update to latest

--- custom-item-price/src/modules/metal-prices/index.ts
+++ custom-item-price/src/modules/metal-prices/index.ts
@@ -1,7 +1,7 @@
 import { Module } from "@medusajs/framework/utils";
 import MetalPricesModuleService from "./service";
 
-export const METAL_PRICES_MODULE = "metal-prices"
+export const METAL_PRICES_MODULE = "metalPrices"
 
 export default Module(METAL_PRICES_MODULE, {
   service: MetalPricesModuleService
