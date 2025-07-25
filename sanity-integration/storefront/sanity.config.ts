c779a1008387f48c055d2870645c2ac30f0b5048
Shahed Nasser <shahednasser@gmail.com>
2025-07-25 11:06:12 +0300

    fix structure sanity import

--- sanity-integration/storefront/sanity.config.ts
+++ sanity-integration/storefront/sanity.config.ts
@@ -6,12 +6,12 @@
 
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
-import {structure as structureTool} from 'sanity/structure'
+import {structureTool} from 'sanity/structure'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './src/sanity/env'
 import {schema} from './src/sanity/schemaTypes'
-import {structure} from './src/sanity/structure'
+import {structure} from './src/sanity/structure.config'
 
 export default defineConfig({
   basePath: '/studio',
