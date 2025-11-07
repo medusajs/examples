2b2cb5655a31424fa4adde0427ad65b3132859ae
Shahed Nasser <shahednasser@gmail.com>
2025-11-07 10:59:40 +0200

    remove unused middleware

--- category-images/src/api/middlewares.ts
+++ category-images/src/api/middlewares.ts
@@ -1,7 +1,9 @@
-import { defineMiddlewares, validateAndTransformBody, validateAndTransformQuery } from "@medusajs/framework"
+import { defineMiddlewares, validateAndTransformBody } from "@medusajs/framework"
 import { CreateCategoryImagesSchema } from "./admin/categories/[category_id]/images/route"
-import { createSelectParams } from "@medusajs/medusa/api/utils/validators"
-import { DeleteCategoryImagesSchema, UpdateCategoryImagesSchema } from "./admin/categories/[category_id]/images/batch/route"
+import { 
+  DeleteCategoryImagesSchema, 
+  UpdateCategoryImagesSchema
+} from "./admin/categories/[category_id]/images/batch/route"
 
 export default defineMiddlewares({
   routes: [
@@ -10,16 +12,6 @@ export default defineMiddlewares({
       method: ["POST"],
       middlewares: [
         validateAndTransformBody(CreateCategoryImagesSchema)
-      ]
-    },
-    {
-      matcher: "/admin/categories/:category_id/images/:id",
-      method: ["GET"],
-      middlewares: [
-        validateAndTransformQuery(createSelectParams(), {
-          isList: false,
-          defaults: ["id", "type", "url", "file_id"],
-        })
       ]
     },
     {
