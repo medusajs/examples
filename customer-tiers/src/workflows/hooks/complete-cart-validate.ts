0fee74b175cfe3b72baf011247b152dc5fb11237
Shahed Nasser <shahednasser@gmail.com>
2025-11-25 17:34:27 +0200

    changes to cart completion hook

--- customer-tiers/src/workflows/hooks/complete-cart-validate.ts
+++ customer-tiers/src/workflows/hooks/complete-cart-validate.ts
@@ -4,19 +4,10 @@ import { MedusaError } from "@medusajs/framework/utils"
 completeCartWorkflow.hooks.validate(async ({ cart }, { container }) => {
   const query = container.resolve("query")
 
-  // Get customer details
-  const data = cart.customer_id ? await query.graph({
-    entity: "customer",
-    fields: ["id", "tier.*"],
-    filters: {
-      id: cart.customer_id,
-    },
-  }) : null
-
   // Get cart with promotions
-  const { data: [cartWithPromotions] } = await query.graph({
+  const { data: [detailedCart] } = await query.graph({
     entity: "cart",
-    fields: ["id", "promotions.*"],
+    fields: ["id", "promotions.*", "customer.id", "customer.tier.*"],
     filters: {
       id: cart.id,
     },
@@ -24,24 +15,24 @@ completeCartWorkflow.hooks.validate(async ({ cart }, { container }) => {
     throwIfKeyNotFound: true,
   })
 
-  if (!cartWithPromotions?.promotions || cartWithPromotions.promotions.length === 0) {
+  if (!detailedCart?.promotions || detailedCart.promotions.length === 0) {
     return
   }
 
   // Get customer's tier
-  const customerTier = data?.data?.[0].tier
+  const customerTier = detailedCart.customer?.tier
 
   // Get all tier promotions to check
   const { data: allTiers } = await query.graph({
     entity: "tier",
     fields: ["id", "promo_id"],
     filters: {
-      promo_id: cartWithPromotions.promotions.map((p) => p?.id).filter(Boolean) as string[]
+      promo_id: detailedCart.promotions.map((p) => p?.id).filter(Boolean) as string[]
     }
   })
 
   // Validate that if a tier promotion is applied, the customer belongs to that tier
-  for (const promotion of cartWithPromotions.promotions) {
+  for (const promotion of detailedCart.promotions) {
     const tierId = allTiers.find((t) => t.promo_id === promotion?.id)?.id
     if (tierId && customerTier?.id !== tierId) {
       throw new MedusaError(
